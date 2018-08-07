2d0da16377893f8bf10854a033f092ab9ef27295
Ashish <ashish.kadyan@comprotechnologies.com>
2018-08-07 11:27:11 +0530

    updated code

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
    
     path1: "D:/Comparer/resources/1a.08.a1",
     path2: "D:/Comparer/resources/1a.08.t1",
-    dest: "E:/Step-XML-Concatenator/resources/assets"
+    dest: "E:/Step-XML-Concatenator/input/assets"
   }
 };
